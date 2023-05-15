4e3fb6fad74fff931fc364b454130c073d9c5812
Cristopher Alexis Zarate Valencia - Lap <fenshu28@gmail.com>
2023-05-15 14:05:56 -0600

    Agregure la clase Paciente

---
+++ src/model/Paciente.java
@@ -0,0 +1,67 @@
+/*****************************************************
+ * Autor: Cristopher Alexis Zarate Valencia          *
+ * Fecha de creación: 21/03/2023                     *
+ * Fecha de actualización: 22/03/2023                *
+ * Descripción: Clase para el paciente		 
+ *****************************************************/
+package model;
+
+import java.time.LocalDate;
+
+public class Paciente {
+    String nombreCompleto;
+    LocalDate fechaNacimiento;
+    String genero;
+    String direccion;
+    String numeroTelefono;
+
+    public Paciente(String nombreCompleto, LocalDate fechaNacimiento, String genero, String direccion, String numeroTelefono) {
+        this.nombreCompleto = nombreCompleto;
+        this.fechaNacimiento = fechaNacimiento;
+        this.genero = genero;
+        this.direccion = direccion;
+        this.numeroTelefono = numeroTelefono;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getNumeroTelefono() {
+        return numeroTelefono;
+    }
+
+    public void setNumeroTelefono(String numeroTelefono) {
+        this.numeroTelefono = numeroTelefono;
+    }
+    
+    
+}
