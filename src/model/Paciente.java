fc67cf30a993fd94d61e41819168c20d51b5374e
Cristopher Alexis Zarate <fenshu28@gmail.com>
2023-05-17 12:45:37 -0600

    Se agregó la clase Persona y se heredo a Paciente

--- src/model/Paciente.java
+++ src/model/Paciente.java
@@ -6,66 +6,6 @@
  *****************************************************/
 package model;
 
-import java.time.LocalDate;
-
-public class Paciente {
-    String nombreCompleto;
-    String fechaNacimiento;
-    String genero;
-    String direccion;
-    String numeroTelefono;
-       
-    public Paciente(){
-        
-    }
-    
-    public Paciente(String nombreCompleto, String fechaNacimiento, String genero, String direccion, String numeroTelefono) {
-        this.nombreCompleto = nombreCompleto;
-        this.fechaNacimiento = fechaNacimiento;
-        this.genero = genero;
-        this.direccion = direccion;
-        this.numeroTelefono = numeroTelefono;
-    }
-
-    public String getNombreCompleto() {
-        return nombreCompleto;
-    }
-
-    public void setNombreCompleto(String nombreCompleto) {
-        this.nombreCompleto = nombreCompleto;
-    }
-
-    public String getFechaNacimiento() {
-        return fechaNacimiento;
-    }
-
-    public void setFechaNacimiento(String fechaNacimiento) {
-        this.fechaNacimiento = fechaNacimiento;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getNumeroTelefono() {
-        return numeroTelefono;
-    }
-
-    public void setNumeroTelefono(String numeroTelefono) {
-        this.numeroTelefono = numeroTelefono;
-    }
-    
+public class Paciente extends Persona {
     
 }
---
+++ src/model/Persona.java
@@ -0,0 +1,69 @@
+/** ***************************************************
+ * Autor: Cristopher Alexis Zarate Valencia          *
+ * Fecha de creación: 17 may 2023                     *
+ * Fecha de actualización: 17 may 2023                *
+ * Descripción: 
+ **************************************************** */
+
+package model;
+
+
+public class Persona {
+    String nombreCompleto;
+    String fechaNacimiento;
+    String genero;
+    String direccion;
+    String numeroTelefono;
+       
+    public Persona(){
+        
+    }
+    
+    public Persona(String nombreCompleto, String fechaNacimiento, String genero, String direccion, String numeroTelefono) {
+        this.nombreCompleto = nombreCompleto;
+        this.fechaNacimiento = fechaNacimiento;
+        this.genero = genero;
+        this.direccion = direccion;
+        this.numeroTelefono = numeroTelefono;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getNumeroTelefono() {
+        return numeroTelefono;
+    }
+
+    public void setNumeroTelefono(String numeroTelefono) {
+        this.numeroTelefono = numeroTelefono;
+    }
+}
